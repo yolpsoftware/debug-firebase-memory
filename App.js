ae8509752c8ad745026bbc34d74ee0f7fe49810e
Jonas Sourlier <jonas.sourlier@yolp.ch>
2022-06-14 22:59:44 +0200

    optimizations

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { initializeApp } from "firebase/app";
 import { collection, doc, getDocs, getFirestore, limit, orderBy, query, setDoc, where } from "firebase/firestore";
@@ -16,42 +16,50 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const COLLECTION_NAME = 'debug-firestore-memory';
+const NUMBER_OF_DOCS_TO_LOAD = 1000;
 
 export default function App() {
-  const [nOfDocs, setNOfDocs] = React.useState(0);
+  const [nOfWrittenDocs, setNOfWrittenDocs] = React.useState(0);
   const createDocs = async () => {
     const CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     const CHARS_LEN = CHARS.length;
     const existingDoc = await getDocs(query(collection(db, COLLECTION_NAME), orderBy('index', 'desc'), limit(1)));
     const existingMaxIndex = existingDoc.docs[0].data().index;
-    for (let i = existingMaxIndex + 1; i < 1000; i++) {
-      const docId = `doc-id-${i.toString().padStart(4, '0')}`;
-      const document = doc(collection(db, COLLECTION_NAME), docId);
-      const data = {
-        id: docId,
-        index: i,
-      };
-      for (let j = 0; j < 100; j++) {
-        let str = 'This string is 1000 bytes together with its key: ';
-        const bytesSoFar = str.length + 1 + 10;
-        for (let k = bytesSoFar; k < 1000; k++) {
-          str += CHARS.charAt(Math.floor(Math.random() * CHARS_LEN));
+    for (let i = existingMaxIndex + 1; i < 4000; i++) {
+      try {
+        const docId = `doc-id-${i.toString().padStart(4, '0')}`;
+        const document = doc(collection(db, COLLECTION_NAME), docId);
+        const data = {
+          id: docId,
+          index: i,
+        };
+        for (let j = 0; j < 100; j++) {
+          let str = 'This string is 1000 bytes together with its key: ';
+          const bytesSoFar = str.length + 1 + 10;
+          for (let k = bytesSoFar; k < 1000; k++) {
+            str += CHARS.charAt(Math.floor(Math.random() * CHARS_LEN));
+          }
+          data[`field-${j.toString().padStart(3, '0')}`] = str;
         }
-        data[`field-${j.toString().padStart(3, '0')}`] = str;
+        await setDoc(document, data);
+        setNOfWrittenDocs(i + 1);
+      } catch (e) {
+        i--;
+        continue;
       }
-      await setDoc(document, data);
-      setNOfDocs(i + 1);
     }
   }
+  // useEffect(() => {
+  //   createDocs();
+  // }, []);
   const [loadedFromFirestore, setLoadedFromFirestore] = useState(null);
   const [docs, setDocs] = useState([]);
-  const LOAD_DOCS = 100;
   const loadDocs = async () => {
     const keys = (await AsyncStorage.getAllKeys())
         .filter(x => x.startsWith('firestore-memory-'));
     debugger;
-    if (keys.length < LOAD_DOCS) {
-      const qr = await getDocs(query(collection(db, COLLECTION_NAME), where('index', '<', LOAD_DOCS)));
+    if (keys.length < NUMBER_OF_DOCS_TO_LOAD) {
+      const qr = await getDocs(query(collection(db, COLLECTION_NAME), where('index', '<', NUMBER_OF_DOCS_TO_LOAD)));
       const loadedDocs = qr.docs;
       setDocs(loadedDocs.map(doc => doc.data()));
       setLoadedFromFirestore(true);
@@ -67,10 +75,7 @@ export default function App() {
   const docsTotalSize = docs.map(x => JSON.stringify(x)).reduce((acc, x) => acc + x.length, 0);
   return (
     <View style={styles.container}>
-      {/* <TouchableOpacity onPress={createDocs} style={{ padding: 8, margin: 8 }}>
-        <Text>Create docs</Text>
-      </TouchableOpacity> */}
-      <Text>{nOfDocs} documents written</Text>
+      {/* <Text>{nOfWrittenDocs} documents written</Text> */}
       <View>
         <Text>Loaded {docs?.length} documents {loadedFromFirestore ? 'from Firestore' : loadedFromFirestore === false ? 'from AsyncStorage' : ''}, total size {docsTotalSize}</Text>
       </View>
